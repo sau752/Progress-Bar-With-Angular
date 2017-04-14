1ae65107a9fc5c1885a0a15a96ce8b036c046ac5
Taruna Bishnoi <tarunab@outlook.com>
2017-04-15 00:27:21 +0530

    Changes

---
+++ script.js
@@ -0,0 +1,22 @@
+var app = angular.module("myApp", []);
+
+app.directive('progressBar', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'progress-bar.html',
+        controller: function ($scope) {
+
+            this.startTime = 0;
+            this.endTime = 100;
+            this.progressValue = 0;
+
+            $scope.$watch("prgbar.progressValue", function (newVal, oldVal) {
+                if (newVal === null)
+                    newVal = 0;
+                $("#progress-elapsed").css("width", newVal + '%');
+                $("#progress-remaining").css("width", (100 - newVal) + '%');
+            });
+        },
+        controllerAs: "prgbar"
+    }
+});
